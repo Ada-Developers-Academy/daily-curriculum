d6c79cd483c0e160c2a6ae19bfb412429df98906
Audrey Carlsen <audreycarlsen@gmail.com>
2013-10-30 14:38:20 -0700

    First commit

--- week1_10.28-11.1/wednesday_13.10.30/calculator.rb
+++ week1_10.28-11.1/wednesday_13.10.30/calculator.rb
@@ -1,26 +1,41 @@
-def self.add_numbers(first, second)
+def add_numbers(first, second)
   first + second
 end
 
-def self.subtract_numbers(first, second)
+def subtract_numbers(first, second)
   first - second
 end
 
-puts "add or subtract?"
-action = gets.downcase
+puts "Add or subtract?"
+action = gets.chomp.downcase
+
+while action != "add" and action != "+" and action != "subtract" and action != "-"
+  puts "C'mon, Dave. Add or subtract?"
+  action = gets.chomp.downcase
+end
 
 puts "First number?"
-first_num = gets.chomp.to_i
+first_num = gets.chomp
+
+while first_num.to_i == 0
+  puts "Please enter an integer, Dave."
+  first_num = gets.chomp
+end
+first_num = first_num.to_i
 
 puts "Second number?"
-second_num = gets.chomp.to_i
+second_num = gets.chomp
 
-if action == "add"
+while second_num.to_i == 0 # and second_num.length > 1
+  puts "Please enter an integer, Dave."
+  second_num = gets.chomp
+end
+second_num = second_num.to_i
+
+if action == "add" or action == "+"
   result = add_numbers(first_num, second_num)
-elsif action == "subtract"
+elsif action == "subtract" or action == "-"
   result = subtract_numbers(first_num, second_num)
-else
-  result = "Sorry, I don't know how to do that, Dave."
 end
 
 puts result
