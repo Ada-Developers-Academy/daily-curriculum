928d8d5e5c761764eb79cc3bc7717ded72b2d540
Kerri Miller <kerrizor@kerrizor.com>
2013-10-30 13:21:56 -0700

    Add calculator.rb

---
+++ week1_10.28-11.1/wednesday_13.10.30/calculator.rb
@@ -0,0 +1,26 @@
+def self.add_numbers(first, second)
+  first + second
+end
+
+def self.subtract_numbers(first, second)
+  first - second
+end
+
+puts "add or subtract?"
+action = gets.chomp.downcase
+
+puts "First number?"
+first_num = gets.chomp
+
+puts "Second number?"
+second_num = gets.chomp
+
+if action == "add"
+  result = add_numbers(first_num, second_num)
+elsif action == "subtract"
+  result = subtract_numbers(first_num, second_num)
+else
+  result = "Sorry, I don't know how to do that, Dave."
+end
+
+puts result
