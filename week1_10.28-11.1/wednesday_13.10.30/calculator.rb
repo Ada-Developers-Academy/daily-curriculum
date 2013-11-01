a6c9c85798572603e43d178c57586b1d9fa37171
Davida Marion <davidamarion@gmail.com>
2013-11-01 15:18:23 -0700

    changed calculator.rb and story.rb

--- week1_10.28-11.1/wednesday_13.10.30/calculator.rb
+++ week1_10.28-11.1/wednesday_13.10.30/calculator.rb
@@ -6,21 +6,103 @@ def subtract_numbers(first, second)
   first - second
 end
 
-puts "add or subtract?"
-action = gets.downcase
-
-puts "First number?"
-first_num = gets.chomp.to_i
-
-puts "Second number?"
-second_num = gets.chomp.to_i
-
-if action == "add" # without chomp action is "add/n"
-  result = add_numbers(first_num, second_num)
-elsif action == "subtract"
-  result = subtract_numbers(first_num, second_num)
-else
-  result = "Sorry, I don't know how to do that, Dave."
+def multiply_numbers(first, second)
+	first * second
 end
 
-puts result
+def divide_numbers(first, second)
+	first.to_f / second.to_f
+end
+
+def expo_numbers(first, second)
+	first ** second
+end
+
+puts "Hi there! What's your name?"
+name = gets.chomp
+if name == ""
+	name = "Dave"
+end
+
+puts "#{name}, what would you like to do? Add, subtract, divide, multiply? Or maybe exponentiate? Or do you want a square root?"
+action = gets.chomp.downcase
+if !["add","+","subtract","-","multiply","*","exponentiate","**","square root","divide","/"].include? action
+	puts "Sorry, I don't know how to do that."
+	exit
+end
+
+puts "First number?"
+first_num = gets.chomp
+while first_num.empty?
+	puts "That's not an appropriate number. Try again!"
+	first_num = gets.chomp
+end
+
+while true
+	begin
+	Float(first_num)
+	break
+	rescue ArgumentError
+	puts "That's not an appropriate number. TRY AGAIN."
+	first_num = gets.chomp
+  end
+end
+
+first_num = first_num.to_i
+
+if action != "square root"
+puts "Second number?"
+second_num = gets.chomp
+while second_num.empty?
+	puts "That's not an appropriate number. Try again!"
+	second_num = gets.chomp
+end
+second_num = second_num.to_i
+end
+
+case action
+when "add","+"
+	result = add_numbers(first_num, second_num)
+  action = "+"
+when "subtract","-"
+	result = subtract_numbers(first_num, second_num)
+	action = "-"
+when "multiply","*"
+	result = multiply_numbers(first_num, second_num)
+	action = "*"
+when "divide","/"
+	result = divide_numbers(first_num, second_num)
+	action = "/"
+when "exponentiate","**"
+	result = expo_numbers(first_num, second_num)
+ 	action = "^"
+when "square root"
+ 	result = Math.sqrt(first_num).to_i
+end
+
+# if ["add", "+"].include? action 
+#   result = add_numbers(first_num, second_num)
+#   action = "+"
+# elsif ["subtract","-"].include? action
+#   result = subtract_numbers(first_num, second_num)
+#   action = "-"
+# elsif ["multiply","*"].include? action
+# 	result = multiply_numbers(first_num, second_num)
+# 	action = "*"
+# elsif ["divide","/"].include? action
+# 	result = divide_numbers(first_num, second_num)
+# 	action = "/"
+# elsif ["exponentiate","**"].include? action
+# 	result = expo_numbers(first_num, second_num)
+# 	action = "^"
+# elsif ["square root"].include? action
+# 	result = Math.sqrt(first_num).to_i
+# else
+#   result = "Sorry, I don't know how to do that, #{name}."
+# end
+
+if action == "square root"
+	puts "The square root of #{first_num} is #{result}"
+else
+	puts "#{first_num} #{action} #{second_num} = #{result}"
+end
---
+++ week1_10.28-11.1/wednesday_13.10.30/my_story.rb
@@ -0,0 +1,6 @@
+puts "It's nighttime, and you're feeling verrrry sleepy. Maybe it's all the cool stuff you've been learning in school; maybe it's the six beers you drank at Standard Brewing. Either way, you slip off to dreamland instantly."
+puts "You wake up with a start! You're not in your bed. Where are you? A park? A ship? A monster's lair?"
+location = gets.chomp
+
+if location == "park"
+	puts "It's broad daylight. You're in Pratt Park, but it's looking very desolate. The fountain isn't running. No children are playing.
