3343c2948f7c33906640665d4493d47bcf14ed73
Kerri Miller <kerrizor@kerrizor.com>
2013-11-05 12:09:38 -0800

    update example objects and add text_analyzer

--- examples/week2/basic_objects/baker.rb
+++ examples/week2/basic_objects/baker.rb
@@ -1,15 +1,19 @@
 class Baker
-  def initialize
+  attr_accessor :eggs, :butter, :rolling_pin
+
+  def initialize(rolling_pin_variable)
     @flour  = 2
     @butter = 5
     @sugar  = 3
     @eggs   = 23
+    @rolling_pin = rolling_pin_variable
   end
 
   def make_cookies
     heat_oven(350)
   end
 
-  def heat_oven(temp)
+  def heat_oven(temperature)
+    puts "The oven is heating up to #{temperature}"
   end
 end
