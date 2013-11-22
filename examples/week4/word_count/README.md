459b7360b297e73ddd86ece124d340fc19edb01a
Kerri Miller <kerrizor@kerrizor.com>
2013-11-22 07:53:07 -0800

    rspec for friday warmup

--- examples/week4/word_count/README.md
+++ examples/week4/word_count/README.md
@@ -1,9 +1,15 @@
+Write a Phrase class that counts the number of times a word is used in a given string, keeping count of each occurrence in a hash.
+
+
 For example for the input `"olly olly in come free"`
 
-```plain
-olly: 2
-in: 1
-come: 1
-free: 1
+```ruby
+{ 'olly' => 2, 'in' => 1, 'come' => 1, 'free' => 1 }
 ```
+
+Do your work in a new git repository, and when you're done, push your code to github and share the URL in Campfire.
+
+If you finish early, help someone else! Mention that you're done and free to help in Campfire, or put your name up on the white board so if anyone in the class could use a hand they'll know they can talk to you.
+
+If you'd like more challenge, I have an additional set of extra credit specs you can take a crack at.
 
--- examples/week4/word_count/spec/lib/phrase_spec.rb
+++ examples/week4/word_count/spec/lib/phrase_spec.rb
@@ -2,68 +2,42 @@ require "./lib/phrase"
 
 describe Phrase do
 
-  def test_count_one_word
+  it "should count a single word one time" do
     phrase = Phrase.new("word")
-    counts = {"word" => 1}
-    assert_equal counts, phrase.word_count
+    counts = { "word" => 1 }
+
+    expect(phrase.word_count).to eq counts
   end
 
-  def test_count_one_of_each
-    skip
+  it "should count each word one time" do
     phrase = Phrase.new("one of each")
-    counts = {"one" => 1, "of" => 1, "each" => 1}
-    assert_equal counts, phrase.word_count
+    counts = { "one" => 1, "of" => 1, "each" => 1 }
+
+    expect(phrase.word_count).to eq counts
   end
 
-  def test_count_multiple_occurrences
-    skip
+  it "should count multiple occurrences one time for each occurrence" do
     phrase = Phrase.new("one fish two fish red fish blue fish")
-    counts = {"one" => 1, "fish" => 4, "two" => 1, "red" => 1, "blue" => 1}
-    assert_equal counts, phrase.word_count
+    counts = { "one" => 1, "fish" => 4, "two" => 1, "red" => 1, "blue" => 1 }
+
+    expect(phrase.word_count).to eq counts
   end
 
-  def test_count_everything_just_once
-    skip
+  it "should reset its counting hash each time it counts words" do
     phrase = Phrase.new("all the kings horses and all the kings men")
-    phrase.word_count # count it an extra time
-    counts = {
-      "all" => 2, "the" => 2, "kings" => 2, "horses" => 1, "and" => 1, "men" => 1
-    }
-    assert_equal counts, phrase.word_count
+
+    # count the words a second time
+    phrase.word_count
+
+    counts = { "all" => 2, "the" => 2, "kings" => 2, "horses" => 1, "and" => 1, "men" => 1 }
+
+    expect(phrase.word_count).to eq counts
   end
 
-  def test_ignore_punctuation
-    skip
-    phrase = Phrase.new("car : carpet as java : javascript!!&@$%^&")
-    counts = {"car" => 1, "carpet" => 1, "as" => 1, "java" => 1, "javascript" => 1}
-    assert_equal counts, phrase.word_count
-  end
-
-  def test_handles_cramped_lists
-    skip
-    phrase = Phrase.new("one,two,three")
-    counts = {"one" => 1, "two" => 1, "three" => 1}
-    assert_equal counts, phrase.word_count
-  end
-
-  def test_include_numbers
-    skip
-    phrase = Phrase.new("testing, 1, 2 testing")
-    counts = {"testing" => 2, "1" => 1, "2" => 1}
-    assert_equal counts, phrase.word_count
-  end
-
-  def test_normalize_case
-    skip
+  it "should normalize the case of the words" do
     phrase = Phrase.new("go Go GO")
-    counts = {"go" => 3}
-    assert_equal counts, phrase.word_count
-  end
+    counts = { "go" => 3 }
 
-  def test_with_apostrophes
-    skip
-    phrase = Phrase.new("First: don't laugh. Then: don't cry.")
-    counts = {"first"=>1, "don't"=>2, "laugh"=>1, "then"=>1, "cry"=>1}
-    assert_equal counts, phrase.word_count
+    expect(phrase.word_count).to eq counts
   end
 end
