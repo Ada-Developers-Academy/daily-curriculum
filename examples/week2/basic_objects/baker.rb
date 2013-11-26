002dd2a9b7ac074a27550fde5abb8b6d3415d659
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-11-25 18:02:34 -0800

    baker example

--- examples/week2/basic_objects/baker.rb
+++ examples/week2/basic_objects/baker.rb
@@ -1,15 +1,47 @@
+require "./oven.rb"
+
 class Baker
-  def initialize
-    @flour  = 2
+  attr_accessor :rolling_pin, :apron
+  attr_reader :eggs, :flour, :butter, :eggs
+
+  def initialize(options_hash)
+    @apron = options_hash[:apron]
+    @rolling_pin = options_hash[:rolling_pin]
+    @flour = 2
+    @sugar = 3
+    @eggs =23
     @butter = 5
-    @sugar  = 3
-    @eggs   = 23
+    @ovens = Oven.new({:type => "gas",
+                      :color => "steel",
+                      :racks => 2})
+  end
+
+  def turn_off_ovens
+    @ovens.each do |oven|
+      oven.on = false
+    end
   end
 
   def make_cookies
-    heat_oven(350)
+    heat_oven(@ovens[0], 375)
   end
 
-  def heat_oven(temp)
+  private
+  def find_a_free_oven
+    free_oven = oven.first
+    return free_oven
+  end 
+
+  def heat_oven(ovens, temperature)
+    ovens.set_temperature(temperature) #not defined, will break code
   end
 end
+
+oven = Oven.new(:color => "yellow")
+new_oven = Oven.new(:color => "steel")
+
+bob = Baker.new({:oven => [oven, new_oven]})
+
+bob.ovens[0].color
+
+bob.ovens.select{ |oven| oven.color == "yellow"}
