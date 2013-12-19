567d6100586deb2d0849f2c6ea1400f2d78c2b17
Kerri Miller <kerrizor@kerrizor.com>
2013-12-19 14:49:01 -0800

    add notes and example code from recursion lecture

---
+++ examples/week8/recursion/hacky.rb
@@ -0,0 +1,11 @@
+class Integer
+  def factorial(num = self)
+    return "Can not calculate factorial of a negative number" if num < 0
+
+    if num <= 1
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
+end
