e49bde4c971c0cf1422e5834e59e260e5c673186
Bookis Smuin <bookis.smuin@gmail.com>
2014-02-24 08:35:14 -0800

    updates weather api example

--- week15/friday/resources/weather_spec.rb
+++ week15/friday/resources/weather_spec.rb
@@ -3,31 +3,42 @@ class Weather
   def initialize(city)
     @city = city
     @weather = get_weather
+    email
   end
   
   def description
     @weather["weather"][0]["description"]
   end
   
+  def email(email_address)
+    WeatherMailer.send_report(email_address).deliver
+  end
+  
   private
   
   def get_weather
+    
     HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{@city}")
   end
 end
 
 describe Weather do
-  let(:weather) { {"weather" => [{"id" => 500,"main" => "Rain","description" => "light rain","icon" => "10d"}],"base" => "cmc stations"}}
+  let(:weather) { {"weather" => [{"description" => "light rain"}]}}
+
+  # it "returns the weather" do
+  #   expect(Weather.new("Seattle").description).to eq "overcast clouds"
+  # end
+
 
   # This is a stub
   it "returns the weather" do
-    HTTParty.stub(:get).with("http://api.openweathermap.org/data/2.5/weather?q=London,uk") { weather }
+    HTTParty.stub(:get) { weather }
     expect(Weather.new("London,uk").description).to eq "light rain"
   end  
   
   # This is a mock
   it "returns the weather" do
-    weather_data = double("weather_data")
+    weather_data = double("data")
     Weather.any_instance.stub(:get_weather) { weather_data }
     expect(weather_data).to receive(:[]).with("weather")     { weather_data }
     expect(weather_data).to receive(:[]).with(0)             { weather_data }
